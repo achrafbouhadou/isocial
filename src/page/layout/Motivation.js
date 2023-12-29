e9dbdc4c1a61b1ebb83e4ec55a5118ba11ee4463
achrafbouhadou <achraf1323@gmail.com>
2023-12-29 22:30:31 +0100

    finish the sign up page for cocalab

---
+++ src/page/layout/Motivation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    
+    <div className='motivation-background pl-[60px] pt-[150px]  justify-center w-1/2 h-screen'>
+        <h1 className='font-semibold text-5xl mb-3 outfit-font text-white '> Join<span className='orange-color'> Cocolab</span>,</h1>
+        
+        <h1 className="font-medium text-2xl w-80 mb-3   outfit-font text-white   ">
+            Where Collaboration Ignites 
+            Entrepreneurship!
+        </h1>
+        <p className='text-gray-300 mb-3 inter-font w-80  text-xs  '>
+        Welcome to Cocolab, the ultimate hub for turning your ideas into reality! Whether you're a skilled creator, a visionary entrepreneur, or both, Cocolab is your gateway to collaborative success.
+        </p>
+    </div>
+  )
+}
