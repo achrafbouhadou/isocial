8c0d4a35fd3ef00cc1b34418e6fbb5f18d07479f
achrafbouhadou <achraf1323@gmail.com>
2024-01-02 13:36:31 +0100

    creat design for add post comp

--- src/Component/AddPost.jsx
+++ src/Component/AddPost.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Avatar } from '@chakra-ui/react'
 
 export default function AddPost() {
   return (
-    <div>AddPost</div>
+    <div className='flex items-center  bg-gray-50 p-5 rounded-lg'>
+       <Avatar size="sm" name='Achraf bouhadou' src='https://bit.ly/tioluwani-kolawole' />
+       <div className='bg-white w-full Inter-font p-5 ml-3 cursor-pointer'>
+          Ask for collab
+       </div>
+    </div>
   )
 }
