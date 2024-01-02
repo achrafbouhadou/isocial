48b80f1843d865e3d6c8fe28ef9cefefe4fd18de
achrafbouhadou <achraf1323@gmail.com>
2024-01-02 10:46:01 +0100

    create header and add the chakra ui kit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './page/Home';
-
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <>
-   <Home />
+    <ChakraProvider>
+        <Home />
+    </ChakraProvider>
     </>
   );
 }
